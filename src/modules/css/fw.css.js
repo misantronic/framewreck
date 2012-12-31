93bce8293c6c93cfa34cb15c44159fd622eb5638
misantronic <david.skx@posteo.de>
2013-01-01 00:48:17 +0100

    minifying

--- src/modules/css/fw.css.js
+++ src/modules/css/fw.css.js
@@ -4,13 +4,15 @@ F.ext({
 	 * @param {String|Object} v object containg CSS-Attributes
 	 * or a string to return the contexts first element CSS-attribute
 	 * @param [x] placeholder for context
+	 * @param [i] placeholder
+	 * @param [j] placeholder
 	 * @returns {String|F}
 	 */
-	css: function(v,x){
+	css: function(v,x,i,j){
 		x=this.x;
 		if(v&&v.big)return getComputedStyle(x[0],null).getPropertyValue(v);
-		for(var i=x[F.L];i--;)
-			for(var j in v) x[i].style[j]=v[j];
+		for(i=x[F.L];i--;)
+			for(j in v)x[i].style[j]=v[j];
 
 		return this
 	}
