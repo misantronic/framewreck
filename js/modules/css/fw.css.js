ee6d1f0790cf0fcc81385127694d054b1acf1125
misantronic <david.skx@posteo.de>
2014-10-22 23:36:39 +0200

    added css module

---
+++ js/modules/css/fw.css.js
@@ -0,0 +1,18 @@
+F.css={
+	/**
+	 * Apply CSS to context
+	 * @param {String|Object} v object containg CSS-Attributes
+	 * or a string to return the contexts first element CSS-attribute
+	 * @returns {String|F}
+	 */
+	css: function(v){
+		if(v&&v.big)return getComputedStyle(this.x[0],null).getPropertyValue(v);
+		for(var i=this.x.length;i--;)
+			for(var j in v) this.x[i].style[j]=v[j];
+
+		return this
+	}
+};
+
+F.extend(F.css);
+
