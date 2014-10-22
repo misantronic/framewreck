c76bb8c70c44833d941831de7843e313facfd068
misantronic <david.skx@posteo.de>
2014-10-22 23:24:17 +0200

    added data module

--- js/fw.core.js
+++ js/fw.core.js
@@ -138,21 +138,6 @@ F=function(c){
 		return this
 	};
 
-
-
-	/**
-	 * Set data for context
-	 * or return its data
-	 * Note: for multiple context elements only
-	 * the first elements data will be returned
-	 * @param [v] value
-	 */
-	_.data=function(v){
-		for(i=l;i--;)
-			if(v)x[i].D=v;
-		return v?this:x[0].D
-	};
-
 	// select context
 	_.find(c);
 
---
+++ js/modules/data/fw.data.js
@@ -0,0 +1,17 @@
+F.data={
+	/**
+	 * Set data for context
+	 * or return its data
+	 * Note: for multiple context elements only
+	 * the first elements data will be returned
+	 * @param [v] value
+	 */
+	data: function(v){
+		for(i=this.x.length;i--;)
+			if(v)this.x[i].D=v;
+		return v?this:this.x[0].D
+	}
+};
+
+F.extend(F.data);
+
