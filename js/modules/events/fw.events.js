23f59f65af8f1b813faa7a408fd03be28da2e50c
misantronic <david.skx@posteo.de>
2014-10-22 23:37:15 +0200

    cleared events module

--- js/modules/events/fw.events.js
+++ js/modules/events/fw.events.js
@@ -6,7 +6,7 @@ F.events={
 	 * @returns {F}
 	 */
 	on: function(v,D){
-		for(i=this.x.length;i--;)
+		for(var i=this.x.length;i--;)
 			this.x[i].addEventListener(v,D);
 
 		return this
@@ -19,7 +19,7 @@ F.events={
 	 * @returns {F}
 	 */
 	off:function(v,D){
-		for(i=this.x.length;i--;)
+		for(var i=this.x.length;i--;)
 			this.x[i].removeEventListener(v,D);
 
 		return this
@@ -32,7 +32,7 @@ F.events={
 	 * @returns {F}
 	 */
 	fire:function(v,D){
-		for(i=this.x.length;i--;)
+		for(var i=this.x.length;i--;)
 			this.x[i].dispatchEvent(new CustomEvent(v,{detail:D}));
 
 		return this
