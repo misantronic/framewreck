446fb0cc4c7895bf49ddd0f2f4fcbc61663674d5
misantronic <david.skx@posteo.de>
2014-10-28 23:50:27 +0100

    optimized animate

--- src/modules/animate/fw.animate.js
+++ src/modules/animate/fw.animate.js
@@ -1,96 +1,63 @@
 F.ext({
 	/**
 	 * Animates the context according to the animation rules
-	 * @param {Array} a Animations
-	 * @param {Function} c Callback
+	 * @param {Array} anims Animations
+	 * @param {Function} callback Callback
+	 * @param [i] placeholder
 	 * @returns {*}
 	 * @see https://github.com/misantronic/framewreck#animate-module
 	 * @example F('#id').animate(['X:100 Y:50', 'O:0.5', 'X:0 Y:0', 'O:1'], function() {
 	 * 	console.log("all done");
 	 * });
 	 */
-	animate: function(a, c) {
+	animate: function(anims, callback, i) {
 		var _ = this;
 
-		_.A = a;
-		_.Ac = c;
+		_.A = anims;
+		_.Ac = callback;
 
-		for(var i = _.x[F.L]; i--;) {
-			this._a(F(_.x[i]), 0);
-		}
+		for(i = _.x[F.L]; i--;)
+			_._a(F(_.x[i]), 0);
 
-		return this
+		return _
 	},
 
 	_a: function(el, i) {
-		var _		= this;
-		var anims	= _.A[i].split(" ");
-		var trs 	= []; // transition
-		var trf		= []; // transform
-		var o		= {};
+		var _		= this,
+			anims	= _.A[i].split(" "),
+			trs 	= [], // transition
+			trf		= [], // transform
+			obj		= {};
 
-		for(var k=0; k < anims.length; k++) {
-			var anim 	= anims[k];
-			var type 	= anim[0];
-			var prop 	= anim.substr(2).split(",");
-			var val 	= prop[0];
+		for(var k=0; k < anims[F.L]; k++) {
+			var anim 	= anims[k],
+				type 	= anim[0],						// animation type
+				prop 	= anim.substr(2).split(","),	// property
+				val 	= prop[0],						// value
+				dur 	= prop[1] || 0.5, 				// duration
+				del 	= parseFloat(prop[2]) || 0;  	// delay
 
-			var dur 	= prop[1] || 0.5; // duration
-			var del 	= parseFloat(prop[2]) || 0; // delay
+			trs[k] = type=='O' ? (obj.opacity = val)&&'opacity '+ dur +'s linear '+ del +'s' : 'transform '+ dur +'s linear '+ del +'s';
 
-			if(type == 'O') {
-				trs[k] = 'opacity '+ dur +'s linear '+ del +'s';
-				o.opacity = val;
-			}
-
-			if(type == 'X') {
-				trs[k] = 'transform '+ dur +'s linear '+ del +'s';
-				trf.push('translateX('+ val +'px)');
-			} else {
-				trf.push('translateX(0px)');
-			}
-
-			if(type == 'Y') {
-				trs[k] = 'transform '+ dur +'s linear '+ del +'s';
-				trf.push('translateY('+ val +'px)');
-			} else {
-				trf.push('translateY(0px)');
-			}
-
-			if(type == 'R') {
-				trs[k] = 'transform '+ dur +'s linear '+ del +'s';
-				trf.push('rotate('+ val +'deg)');
-			} else {
-				trf.push('rotate(0deg)');
-			}
-
-			if(type == 'S') {
-				trs[k] = 'transform '+ dur +'s linear '+ del +'s';
-				trf.push('scale('+ val +')');
-			} else {
-				trf.push('scale(1)');
-			}
+			trf.push('translateX('+ (type == 'X' ? val : 0) +'px)');
+			trf.push('translateY('+ (type == 'Y' ? val : 0) +'px)');
+			trf.push('rotate('+ (type == 'R' ? val : 0) +'deg)');
+			trf.push('scale('+ (type == 'S' ? val : 1) +')');
 		}
 
 		if(trs.length)
-			o.transition = trs.join(",");
+			obj.transition = trs.join(",");
 
 		if(trf.length)
-			o.transform = trf.join(" ");
+			obj.transform = trf.join(" ");
 
-		function handler() {
-			el.off('transitionend', handler);
+		function h() {
+			el.off('transitionend', h);
 
-			if(_.A[++i]) {
-				_._a(el, i);
-			} else {
-				_.Ac&&_.Ac.call(_);
-			}
+			_.A[++i]?_._a(el, i):_.Ac&&_.Ac.call(_)
 		}
 
-		el
-			.css(o)
-			.on('transitionend', handler);
+		el.on('transitionend', h).css(obj)
 	}
 });
 //F.vendor = (Array.prototype.slice.call(getComputedStyle(F.d.documentElement, '')).join('').match(/-(moz|webkit|ms)-/) || (styles.OLink === '' && ['', 'o']))[1];
