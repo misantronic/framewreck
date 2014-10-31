af750a6769c9603ddeefeff3ac229aa9700ae80b
misantronic <david.skx@posteo.de>
2014-10-31 20:40:49 +0100

    recompiled dist scripts

--- dist/framewreck.js
+++ dist/framewreck.js
@@ -28,7 +28,7 @@ F = function (c) {
 	 * @returns {F}
 	 */
 	_.each = function (v) {
-		for (i = l; i--;)v(_[i], i);
+		for (i = l; i--;)v(F(_[i]), i);
 		return _
 	};
 
@@ -187,12 +187,12 @@ F.ext({
 	html: function (v, x) {
 		x = this.x;
 		var a = [], e, g;
-		for (var i = 0; i < x[F.L]; i++)
-			e = x[i],											// assign e as current element
-			g = F.H,											// store innerHTML in g, as it might change
-			e.tagName.match(/INP|SEL|TEX/) && (g = "value"),	// if e is input, select or textarea change g to "value"
-			a.push(e[g]),										// save html/value in array
-			v != []._ && (e[g] = v);							// when v is set, assign new value to element
+		for (var A = [], i = 0; i < x[F.L]; i++)
+			e = x[i],
+			g = F.H,
+			e.tagName.match(/INP|SEL|TEX/) && (g = "value"),
+			e.multiple ? F(e).find('option:checked').each(function(item){ A.push(item.val()) }) && a.push(A) : a.push(e[g]),
+			v != []._ && (e[g] = v);
 		return v && this || a.join("").replace(/\s/g, "")
 	},
 
