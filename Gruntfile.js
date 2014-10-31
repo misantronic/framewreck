0828df045c15505729247a88c527c8fa946a98aa
misantronic <david.skx@posteo.de>
2014-10-31 15:55:26 +0100

    added v.1.2.6: + excluded animate module from dist/framework.*.js + removed banner from modules in dist

--- Gruntfile.js
+++ Gruntfile.js
@@ -26,12 +26,30 @@ module.exports = function(grunt) {
 
 		uglify: {
 			options: {
-				banner: '/*! <%= pkg.name %> <%= pkg.version %> build on <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+				mangle: {
+					except: ['F', '$M']
+				}
 			},
+
 			dist: {
+				options: {
+					banner: '/*! <%= pkg.name %> <%= pkg.version %> build on <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+				},
 				files: {
 					'dist/<%= pkg.name %>.min.js'		: ['<%= concat.dist.dest %>'],
 
+					'dist/modules/core/fw.core.min.js'			: ['src/modules/core/fw.core.js'],
+					'dist/modules/ajax/fw.ajax.min.js'			: ['src/modules/ajax/fw.ajax.js'],
+					'dist/modules/animate/fw.animate.min.js'	: ['src/modules/animate/fw.animate.js'],
+					'dist/modules/css/fw.css.min.js'			: ['src/modules/css/fw.css.js'],
+					'dist/modules/data/fw.data.min.js'			: ['src/modules/data/fw.data.js'],
+					'dist/modules/dom/fw.dom.min.js'			: ['src/modules/dom/fw.dom.js'],
+					'dist/modules/events/fw.events.min.js'		: ['src/modules/events/fw.events.js']
+				}
+			},
+
+			modules: {
+				files: {
 					'dist/modules/core/fw.core.min.js'			: ['src/modules/core/fw.core.js'],
 					'dist/modules/ajax/fw.ajax.min.js'			: ['src/modules/ajax/fw.ajax.js'],
 					'dist/modules/animate/fw.animate.min.js'	: ['src/modules/animate/fw.animate.js'],
