636a12a6a0f55e544914ead89b0535aa3d96a100
misantronic <david.skx@posteo.de>
2014-11-02 23:53:48 +0100

    css module: added key - value parameter

--- dist/framewreck.js
+++ dist/framewreck.js
@@ -168,18 +168,23 @@ F.ext = function (m) {
 F.ext({
 	/**
 	 * Apply CSS to context
-	 * @param {String|Object} v object containg CSS-Attributes
+	 * @param {String|Object} o object containg CSS-Attributes
+	 * or a string containg the property name
 	 * or a string to return the contexts first element CSS-attribute
+	 * @param {String} [v] value for the property
 	 * @param [x] placeholder for context
 	 * @param [i] placeholder
 	 * @param [j] placeholder
 	 * @returns {String|F}
 	 */
-	css: function (v, x, i, j) {
+	css: function (o, v, x, i, j) {
 		x = this.x;
-		if (v && v.big)return getComputedStyle(x[0], null).getPropertyValue(v);
+		if (o && o.big && v == []._) return getComputedStyle(x[0], null).getPropertyValue(o);
 		for (i = x[F.L]; i--;)
-			for (j in v)x[i].style[j] = v[j] + (!isNaN(v[j]) && j != 'opacity' ? 'px' : '');
+			if(v != []._)
+				x[i].style[o] = v;
+			else
+				for (j in o) x[i].style[j] = o[j] + (!isNaN(o[j]) && j != 'opacity' ? 'px' : '');
 
 		return this
 	}
