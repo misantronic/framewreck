9b908ea2503ac436a3140a23973f478708117b36
misantronic <david.skx@posteo.de>
2014-11-04 23:45:28 +0100

    parse append() elements from 0-n, trim html() string content

--- src/modules/dom/fw.dom.js
+++ src/modules/dom/fw.dom.js
@@ -16,7 +16,7 @@ F.ext({
 				? F(e).find(':'+F.C).each(function(item){ A.push(item.val()) }) && a.push(A)
 				: a.push(e[g]),
 			v != []._ && (e[g] = v);
-		return v && this || a.join("")
+		return v && this || a.join("").trim()
 	},
 
 	/**
@@ -31,7 +31,7 @@ F.ext({
 	append: function (v, x, i) {
 		x = this.x;
 		var g = v[0]=='<' ? 0 : F.d[F.Q](v)[0];
-		for (i = x[F.L]; i--;)
+		for (i = 0; i < x[F.L]; i++)
 			g ? g.appendChild(x[i]) : x[i][F.H] += v;
 
 		return this
--- src/modules/templates/fw.templates.js
+++ src/modules/templates/fw.templates.js
@@ -1,31 +1,24 @@
 F.ext({
-	parseTemplate: function(c) {
-		var _ = this;
-		var h = _.html();
+	parse: function(c) {
+		var _ = this, h = _.html(), s, r;
 
 		// vars
-		_.html(
-			h.replace(/{{\s*(\w+)\s*}}/ig, function(p, $1) {
+		h = h[r="replace"](/{{\s*(\w+)\s*}}/ig, function(p, $1) {
 				return c[$1]
-			})
-		);
-
-		h = _.html();
+			});
 
 		// each
-		_.html(
-			h.replace(/{{#each(?:\s*)(\w+)(?:\s*)}}\n*\t*(.*)\n*\t*\{{\/each}}/ig, function(p, $1, $2) {
-				var items = [];
-				c[$1].forEach(function(item, i) {
-					items[i] = $2.replace(/{{#\s*(\w+)\s*}}/ig, function(p2, $3) {
-						if($3 == 'value') return c[$1];
-						return c[$1][i][$3];
+		h = h[r](/{{#each(?:\s*)(\w+)(?:\s*)}}\n*\t*(.*)\n*\t*\{{\/each}}/ig, function(p, $1, $2) {
+				s = "";
+				c[$1].forEach(function(item, i, a) {
+					s += $2[r](/{{#\s*(\w+)\s*}}/ig, function(p2, $3) {
+						a = c[$1][i];
+						return $3 == 'value' ? a : a[$3]
 					})
 				});
-				return items.join("");
-			})
-		);
+				return s
+			});
 
-		return _
+		return F(h.trim())
 	}
 });
