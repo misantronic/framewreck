b1b5307d9aab32bceff3c3a4424643800d52625f
misantronic <david.skx@posteo.de>
2014-11-04 20:53:06 +0100

    templates: added array containg objects

--- src/modules/templates/fw.templates.js
+++ src/modules/templates/fw.templates.js
@@ -10,12 +10,17 @@ F.ext({
 			})
 		);
 
+		h = _.html();
+
 		// each
 		_.html(
-			h.replace(/{{#each(?:\s*)(\w+)(?:\s*)}}\n\t*(.*)\n*\t*\{{\/each}}/ig, function(p, $1, $2) {
+			h.replace(/{{#each(?:\s*)(\w+)(?:\s*)}}\n*\t*(.*)\n*\t*\{{\/each}}/ig, function(p, $1, $2) {
 				var items = [];
 				c[$1].forEach(function(item, i) {
-					items[i] = $2.replace("{{#value}}", item);
+					items[i] = $2.replace(/{{#\s*(\w+)\s*}}/ig, function(p2, $3) {
+						if($3 == 'value') return c[$1];
+						return c[$1][i][$3];
+					})
 				});
 				return items.join("");
 			})
