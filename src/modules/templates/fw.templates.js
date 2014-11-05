e8b9c540e935e16ba9b3efafd85c144e1d6e3c13
misantronic <david.skx@posteo.de>
2014-11-05 12:53:36 +0100

    optimized template parsing

--- src/modules/templates/fw.templates.js
+++ src/modules/templates/fw.templates.js
@@ -1,29 +1,29 @@
 F.ext({
 	parse: function(c) {
-		var h = this.html(), s, r,
+		var h = this.html(), s, r, v = /{+\{ *(\w+) *}}+/g,
 			/**
 			 * escape {{{ }}} templates
 			 */
 			e = function(u, v) {
-				return u[2] == '{' ? new Option(v)[F.H] : v;
+				return u.substr(0, 3) == '{{{' && u.slice(-3) == '}}}' ? new Option(v)[F.H] : v;
 			};
 
-		// vars
-		h = h[r="replace"](/{*\{{\s*(\w+)\s*}}}*/g, function(p, $1) {
-				return e(p, c[$1])
-			});
-
 		// each
-		h = h[r](/{{#each(?:\s*)(\w+)(?:\s*)}}([\s\S]*){{\/each}}/ig, function(p, $1, $2) {
-				s = "";
-				c[$1].forEach(function(item, i, a) {
-					s += $2[r](/{*\{{#*\s*(\w+)\s*}}}*/g, function(p2, $3) {
-						a = c[$1][i];
-						a = $3 == 'value' ? a : a[$3];
-						return e(p2, a)
-					})
-				});
-				return s
+		h = h[r="replace"](/{{#each(?: *)(\w+)(?: *)}}([\s\S]*){{\/each}}/ig, function(p, $1, $2) {
+			s = "";
+			c[$1].forEach(function(item, i, a) {
+				s += $2[r](v, function(p2, $3) {
+					a = c[$1][i];
+					a = $3 == 'value' ? a : a[$3];
+					return e(p2, a)
+				})
+			});
+			return s
+		});
+
+		// vars
+		h = h[r](v, function(p, $1) {
+				return e(p, c[$1])
 			});
 
 		return F(h.trim())
